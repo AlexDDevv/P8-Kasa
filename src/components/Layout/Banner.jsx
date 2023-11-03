eab486ebcbf7e277ddb9b0287d4968f694763662
Alexis <AlexDdevs@gmail.com>
2023-11-03 17:41:20 +0100

    Component pour afficher les banner selon la page

---
+++ src/components/Layout/Banner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Banner(props) {
+  return (
+    <div className="banner-container">
+        <h1 className="banner-title">{props.title}</h1>
+        <img src={props.src} alt="Bannière Kasa" />
+    </div>
+  )
+}
