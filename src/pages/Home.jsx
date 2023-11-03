99d7922effa6e2a262adc092acc94dbf9b671de0
Alexis <AlexDdevs@gmail.com>
2023-11-03 17:44:00 +0100

    Component pour afficher la home banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Header from '../components/Layout/Header'
+import HomeBanner from '../components/Home/HomeBanner'
 
 export default function Home() {
-  return (
+  return <>
     <Header />
-  )
+    <HomeBanner />
+  </>
 }
