d0eb006709b88373d89c74dfc683f2f30fb5c2cc
Alexis <AlexDdevs@gmail.com>
2023-11-03 15:23:30 +0100

    create the header

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <header className='header'>
+        <div className="header-logo">
+            <img src="../src/assets/images/logo/logo-desktop.png" alt="Logo de l'entreprise Kasa" className="logo-header" />
+        </div>
+        <nav className="nav-bar">
+            <ul className="nav-list">
+              <li className="nav-li">
+                <NavLink to="/" className="nav-link" activeClassName="active-nav-link">Accueil</NavLink>
+              </li>
+              <li className="nav-li">
+                <NavLink to="/About" className="nav-link" activeClassName="active-nav-link">À propos</NavLink>
+              </li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
