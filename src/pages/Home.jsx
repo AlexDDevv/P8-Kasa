762fba9f955e982e8bc3b05c7bd65d6dff6c9dff
Alexis <AlexDdevs@gmail.com>
2023-11-05 10:16:40 +0100

    Ajout du component footer à la page Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from '../components/Layout/Header'
 import HomeBanner from '../components/Home/HomeBanner'
 import DisplayCard from '../components/Home/DisplayCard'
+import Footer from '../components/Layout/Footer'
 
 export default function Home() {
   return <>
     <Header />
     <HomeBanner />
     <DisplayCard />
+    <Footer />
   </>
 }
