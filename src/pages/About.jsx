2221fc7d01922e382a9bbad2e04d31d274b7de39
Alexis <AlexDdevs@gmail.com>
2023-11-08 18:13:04 +0100

    Ajout du composant des menus à la page About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../components/Layout/Header'
 import AboutBanner from '../components/About/AboutBanner'
+import DisplayCollapse from '../components/Layout/DisplayCollapse'
+import Footer from '../components/Layout/Footer'
 
 export default function About() {
   return <>
     <Header />
     <AboutBanner />
+    <DisplayCollapse />
+    <Footer />
   </>
 }
