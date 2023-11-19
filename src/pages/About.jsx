29e08a317faf42c4f148104626c0bef7a29e6456
Alexis <AlexDdevs@gmail.com>
2023-11-19 16:33:02 +0100

    Modif pour appeler le bon fichier

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Header from '../components/Layout/Header'
 import AboutBanner from '../components/About/AboutBanner'
-import DisplayCollapse from '../components/Layout/DisplayCollapse'
+import AboutContent from '../components/About/AboutContent'
 import Footer from '../components/Layout/Footer'
 
 export default function About() {
   return <>
     <Header />
     <AboutBanner />
-    <DisplayCollapse />
+    <AboutContent />
     <Footer />
   </>
 }
