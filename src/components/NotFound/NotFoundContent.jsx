47b578ca52e771b3885fcdf382fb1631e283f386
Alexis <AlexDdevs@gmail.com>
2024-01-25 10:40:09 +0100

    Link to="/" not to="/home"

--- src/components/NotFound/NotFoundContent.jsx
+++ src/components/NotFound/NotFoundContent.jsx
@@ -7,7 +7,7 @@ export default function NotFoundContent() {
         <div className="erreur-content">
             <h1 className="erreur-title">404</h1>
             <p className="erreur-p">Oups! La page que vous demandez n'existe pas.</p>
-            <Link to="/home" className="return-home">Retourner sur la page d'accueil</Link>
+            <Link to="/" className="return-home">Retourner sur la page d'accueil</Link>
         </div>
     </section>
   )
