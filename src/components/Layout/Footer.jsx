e8741099041e9e41bb936d608716677175766b05
Alexis <AlexDdevs@gmail.com>
2024-02-05 18:18:12 +0100

    wrong path for logo

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="footer">
         <div className="footer-content">
-            <img src="../src/assets/images/logo/logo-footer.png" alt="Logo Kasa en blanc" className="logo-footer"/>
+            <img src="../../src/assets/images/logo/logo-footer.png" alt="Logo Kasa en blanc" className="logo-footer"/>
             <p className="copyright">© 2020 Kasa. All rights reserved</p>
         </div>
     </footer>
